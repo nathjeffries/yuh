bee39807dc88e3b45f60cbd3feac60d0e6c0c1d9
unknown <jeffrnat000@warren.k12.in.us>
2022-02-01 12:06:33 -0500

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+
+public class Car {
+
+    String make = "Honda";
+    String model = "Civic";
+   static int year = 2001;
+
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        System.out.println(year + " " + car1.make + " " + car1.model);
+    }
+
+}
---
+++ src/org/WalkerCC_CompSci/Square.java
@@ -0,0 +1,19 @@
+package org.WalkerCC_CompSci;
+
+public final class Square extends Rectangle{
+
+    public Square(double sideLength) {
+        super(4,4);  //What goes here?
+
+    }
+
+    public double getSideLength(){
+        return this.length;
+    }
+
+    public void setSideLength(double width){
+       this.width = width; //What goes here?
+    }
+
+
+}
